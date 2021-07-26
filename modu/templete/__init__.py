946c1cbddc2102cf3a892c36f2e0d2fffc663f5d
lmn165 <lastalking@naver.com>
2021-07-26 17:48:21 +0900

    July 26 Multi-Conditional Graphs

--- modu/templete/__init__.py
+++ modu/templete/__init__.py
@@ -1,10 +1,11 @@
 from common.menu import print_menu
 from modu.templete.basic_plot import plot_show, plot_two_list_show, plot_legend, plot_color, plot_linestyle, plot_scatter
+from modu.templete.changed_temperatures_on_my_birthday import ChangedTemperaturesOnMyBirthday
 
 if __name__ == '__main__':
     while 1:
         menu = print_menu(
-            ['exit', 'Plot Show', 'Plot_two_list_show', 'Plot_legend', 'Plot_color', 'Plot_line_style', 'Plot_scatter'])
+            ['exit', 'Plot Show', 'Plot_two_list_show', 'scatter', 'Blank', 'ChangedTemperaturesOnMyBirthday'])
         if menu == 0:
             break
         if menu == 1:
@@ -12,10 +13,8 @@ if __name__ == '__main__':
         if menu == 2:
             plot_two_list_show()
         if menu == 3:
-            plot_legend()
-        if menu == 4:
-            plot_color()
-        if menu == 5:
-            plot_linestyle()
-        if menu == 6:
             plot_scatter()
+        if menu == 4:
+            pass
+        if menu == 5:
+            ChangedTemperaturesOnMyBirthday().processing()
